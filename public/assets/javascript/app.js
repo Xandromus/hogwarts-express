92ff6eb5c7b0d4461e10547d5abe223acb50afba
Xandromus <xanderrapstine@gmail.com>
2018-03-15 18:42:03 -0500

    commented js code

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -1,5 +1,6 @@
 (function() {
 
+    // global variables
     var trainName;
     var destination;
     var firstTrainTime;
@@ -10,7 +11,7 @@
 
     var database = firebase.database();
 
-    // Empties the user input areas
+    // function to empty the user input areas
     function emptyInput() {
         $("#trainName").val("");
         $("#destination").val("");
@@ -21,14 +22,16 @@
     // Adds click functionality to submit button
     $("#add-train-form").on("submit", function(e) {
 
+        // stop the form submit
         e.preventDefault();
 
-        // Assigns user input to variables
+        // assigns user input to variables
         trainName = $("#trainName").val().trim().toLowerCase();
         destination = $("#destination").val().trim().toLowerCase();
         firstTrainTime = $("#firstTrainTime").val().trim();
         frequency = $("#frequency").val().trim();
 
+        // object to hold train information
         var newTrain = {
             name: trainName,
             destination: destination,
@@ -37,58 +40,64 @@
             dateAdded: firebase.database.ServerValue.TIMESTAMP
         };
 
+        // push train information into Firebase
         database.ref("trains/").push(newTrain);
 
+        // call function to empty user input areas
         emptyInput();
     });
 
-
+    // function to add Firebase information to table each time a new train is added
     database.ref("trains/")
         .orderByChild("dateAdded")
         .on("child_added", function(snapshot) {
-            // storing the snapshot.val() in a variable for convenience
-            console.log(key);
-            var sv = snapshot.val();
-            key = snapshot.key;
-            console.log(key);
 
+            // store the snapshot.val() in a variable for convenience
+            var sv = snapshot.val();
+
+            // store the snapshot key in a variable
+            key = snapshot.key;
+
+            // assign snaphot information to variables
             trainName = snapshot.val().name;
             destination = snapshot.val().destination;
             firstTrainTime = snapshot.val().first;
             frequency = snapshot.val().frequency;
 
-            // First Time (pushed back 1 year to make sure it comes before current time)
-        var firstTimeConverted = moment(firstTrainTime, "HH:mm").subtract(1, "years");
-        console.log(firstTimeConverted);
+            // first Time (pushed back 1 year to make sure it comes before current time)
+            var firstTimeConverted = moment(firstTrainTime, "HH:mm").subtract(1, "years");
 
-        // Current Time
-        var currentTime = moment();
-        console.log("CURRENT TIME: " + currentTime.format("HH:mm"));
+            // Current Time
+            var currentTime = moment();
 
-        // Difference between the times
-        var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-        console.log("DIFFERENCE IN TIME: " + diffTime);
+            // calculate ifference between the times
+            var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
 
-        // Time apart (remainder)
-        var remainder = diffTime % frequency;
-        console.log(remainder);
+            // calculate remainder
+            var remainder = diffTime % frequency;
 
-        // Minute Until Train
-        minutesAway = frequency - remainder;
-        console.log("MINUTES TILL TRAIN: " + minutesAway);
+            // calculate minutes until train
+            minutesAway = frequency - remainder;
 
-        // Next Train
-        nextArrival = moment().add(minutesAway, "minutes").format("h:mm A");
-        console.log("ARRIVAL TIME: " + nextArrival);
+            // calculate next train arrival
+            nextArrival = moment().add(minutesAway, "minutes").format("h:mm A");
 
-            // Change the HTML to reflect
+            // update the HTML table to reflect the added child
+
+            // create variables for table cell information
             var nameCell = $("<td class='capitalize'>").text(sv.name);
             var destCell = $("<td class='capitalize'>").text(sv.destination);
             var frequencyCell = $("<td>").text(sv.frequency);
             var nextCell = $("<td>").text(nextArrival);
             var minutesCell = $("<td>").text(minutesAway);
+
+            // create button to remove train from table and database
             var remove = $("<td class='text-center'><button type='submit' class='remove btn btn-primary btn-sm'><i class='fa fa-trash'></i> Remove</button></td>");
+
+            // variable to create table row and append table cells
             var newRow = $("<tr>").append(nameCell, destCell, frequencyCell, nextCell, minutesCell, remove).attr("data-id", key);
+
+            // add row to trains table
             $("#trains").append(newRow);
 
             // Handle the errors
@@ -96,11 +105,10 @@
             console.log("Errors handled: " + errorObject.code);
         });
 
+    // function to remove table row and remove train from Firebase
     $("body").on("click", ".remove", function() {
         $(this).closest("tr").remove();
         var id = $(this).closest("tr").data("id");
-        console.log(id);
-        //console.log(database.ref("trains/" + id).child(key).child(key));
         database.ref("trains/").child(id).remove();
     });
 
